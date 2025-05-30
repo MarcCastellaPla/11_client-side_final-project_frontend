1c0c89479d3baf3fc7c60601d16c3c0c6220a253
MarcCastellaPla <145784203+MarcCastellaPla@users.noreply.github.com>
2025-05-30 19:20:41 +0200

    fix: console css error

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./sanitize.css";
-import "./globals.css";
-import "./index.css";
 import App from "./components/App";
 
 createRoot(document.getElementById("root")).render(
